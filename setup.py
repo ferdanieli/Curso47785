546e8b6f9b5a49cc8afb219e77698dc14e604498
Fernanda Danieli <ferdanieli.danieli@gmail.com>
2023-11-01 22:05:12 -0300

    agregando archivo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="primer_paquete",
+    version="1.0",
+    description="",
+    author="Luis",
+    author_email="Luis@gmail.com",
+    packages=["calculadoras"]
+)
