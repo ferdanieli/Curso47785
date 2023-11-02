169a75914f222ccb6e4617291582c0ca327d171e
Fernanda Danieli <ferdanieli.danieli@gmail.com>
2023-11-01 21:19:39 -0300

    guardar archivo main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from resultados.resultados_calculadora import resultado_suma
+
+from calculadoras.cientifica import tangente, nombre
+
+print(resultado_suma)
+
+print(tangente("esta es mi tangente"))
+
+print(nombre)
