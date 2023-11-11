7e146e1efb2368708519de532891018d7c2e3811
Fernanda Danieli <ferdanieli.danieli@gmail.com>
2023-11-11 10:11:56 -0300

    guardar el archivo de calculadoras

---
+++ calculadoras/basica.py
@@ -0,0 +1,15 @@
+def suma(*args):
+    resultado = 0
+    for val in args:
+        resultado += val
+    return resultado
+
+def multiplicar(*args):
+    resultado = 0
+    return resultado
+
+def dividir(num1, num2):
+    return num1 / num2
+
+
+
---
+++ calculadoras/cientifica.py
@@ -0,0 +1,4 @@
+def tangente(num1):
+    return num1
+
+nombre = "Luisgit"
